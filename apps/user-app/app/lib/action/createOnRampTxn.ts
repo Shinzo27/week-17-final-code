92fceeff32eeb3548d7d9c6892cf69ce20227255
Shinzo27 <20bmiit031@gmail.com>
2024-08-06 14:04:01 +0530

    onRamp

--- apps/user-app/app/lib/action/createOnRampTxn.ts
+++ apps/user-app/app/lib/action/createOnRampTxn.ts
@@ -15,7 +15,7 @@ export async function createOnRampTransaction(amount: number, provider: string){
     }
     await prisma.onRampTransaction.create({
         data: {
-            userId,
+            userId: Number(userId),
             amount: amount,
             status: "Processing",
             startTime: new Date(),
